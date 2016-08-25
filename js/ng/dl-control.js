dc6284c47b5f86867acbf004dd640db0d054a000
David Vierra <codewarrior@hawaii.rr.com>
2016-08-24 17:32:32 -1000

    testing downloads 3

--- js/ng/dl-control.js
+++ js/ng/dl-control.js
@@ -4,7 +4,7 @@ var MODEL_URL = 'https://api.github.com/repos/mcedit/mcedit2/releases';
 
 dlApp.controller('DownloadListCtrl', function ($scope, $http) {
     $http.get(MODEL_URL).success(function(data) {
-        $scope.downloadData = data
+        $scope.downloads = data
     })
 
 });
